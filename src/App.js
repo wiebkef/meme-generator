1e75002e8268a21361cec4aa4a2fc14825dcf457
Wiebke <wiebke.feldt@gmail.com>
2023-02-22 10:58:34 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Draggable from "react-draggable";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Stack from "react-bootstrap/Stack";
 
 function App() {
+  const [memes, setMemes] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const randomMeme = Math.floor(Math.random() * (memes.length - 1));
+  console.log(randomMeme);
+
+  useEffect(() => {
+    memes.length === 0 &&
+      axios
+        .get("https://api.imgflip.com/get_memes")
+        .then((response) => {
+          console.log(response.data.data.memes);
+          setMemes(response.data.data.memes);
+          setLoading(false);
+        })
+        .catch((err) => console.log(err));
+  });
+
+  const getBoxCount = (count) => {
+    let boxes = [];
+    for (let i = 0; i < count; i++) {
+      console.log("COUNT: ", i);
+      boxes.push(
+        <Draggable>
+          <input key={i} placeholder={i + 1}></input>
+        </Draggable>
+      );
+    }
+    return boxes;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!loading && (
+        <Row>
+          <Col>
+            <img src={`${memes[randomMeme].url}`} alt="a random meme" />
+          </Col>
+          <Col>
+            <Stack gap={1}>
+              <h3>Write your own meme</h3>
+              {console.log("HUUUUU", memes[randomMeme].box_count)}
+              {getBoxCount(memes[randomMeme].box_count)}
+            </Stack>
+          </Col>
+        </Row>
+      )}
     </div>
   );
 }
